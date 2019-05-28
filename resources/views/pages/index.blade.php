9899ae86e7f703d1b54b90696715c94974eb89ee
MariahFarnsworth <mariahfarnsworth33@gmail.com>
2019-05-28 12:30:34 -0400

    Part 3: Basic routing and controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('APP_NAME', 'LSAPP')}}</title>
+ 
+    </head>
+    <body>
+      <h1>Welcome to laravel</h1>
+      <p>This is the laravel application from the "Laravel From Scratch" Youtube series.</p>
+    </body>
+</html>
